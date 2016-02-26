6806ebe2514fe8ec4035ec5c8a8164889db6a659
Tom <tom.deblende@gmail.com>
2016-02-26 19:00:29 +0100

    First commit.

---
+++ storage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+volumes = ec2.volumes.all()
+
+total = 0
+
+for v in volumes: 
+  if 'Name' in str(v.tags):
+    vn = [tag['Value'] for tag in v.tags if tag['Key'] == 'Name'][0]
+    print vn + "\t" + str(v.size)
+    total = total + v.size
+print "Total:" + "\t" +  str(total)
+
