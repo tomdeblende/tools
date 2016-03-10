f89db203d3ede5e141e2035bbcb468ee7a00279d
Tom <tom.deblende@gmail.com>
2016-03-10 14:20:39 +0100

    List  instances in csv.

---
+++ instances.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+instances = ec2.instances.all()
+
+total = 0
+
+print "instance-id,instance name,application,environment"
+
+for i in instances:
+    inn = [tag['Value'] for tag in i.tags if tag['Key'] == 'Name'][0]
+    ine = [tag['Value'] for tag in i.tags if tag['Key'] == 'environment'][0]
+    ina = [tag['Value'] for tag in i.tags if tag['Key'] == 'application'][0]
+    print i.id + "," + inn + "," + ina + "," + ine
+
