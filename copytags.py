cf3acfc93e93fc813d77395e9028e63e1de7926e
Tom <tom.deblende@gmail.com>
2016-08-30 12:56:16 +0200

    copytags

---
+++ copytags.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import boto3
+import time
+import sys
+
+ec2 = boto3.resource('ec2')
+source_instance = ec2.Instance(str(sys.argv[0]))
+target_instance = ec2.Instance(str(sys.argv[1]))
+
+
+if source_instance.tags is not None:
+    try:
+        for tag in source_instance.tags:
+            if tag['Key'] != 'Name':
+                target_instance.create_tags(Tags=tag)
+    except Exception, e:
+        print ('Error message: {}'.format(e))
+        time.sleep(5)
